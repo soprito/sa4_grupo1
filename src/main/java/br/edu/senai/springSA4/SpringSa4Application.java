51684c32bc928b2335e6d18cc2cda88ccb004558
soprito <soprito@gmail.com>
2023-10-06 17:40:00 -0300

    V.0.0.2 Projeto inicial

---
+++ src/main/java/br/edu/senai/springSA4/SpringSa4Application.java
@@ -0,0 +1,13 @@
+package br.edu.senai.springSA4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSa4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSa4Application.class, args);
+	}
+
+}
---
+++ src/test/java/br/edu/senai/springSA4/SpringSa4ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.senai.springSA4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSa4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
